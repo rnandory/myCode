476f905b0ebf616da89eaa544d18f0a12b05330a
rnandory <tntjs2637@gmail.com>
2024-07-25 15:18:41 +0900

    0725 dual pivot quicksort 완료

--- codeJourney/src/main/java/mySort/DualPivotQuick.java
+++ codeJourney/src/main/java/mySort/DualPivotQuick.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class DualPivotQuick {
 	
 	public static void dualPivotQuickSort(int[] arr, int low, int high) {
-		if (low > high)
+		if (low >= high)
 			return;
 		
 		int[] pivot = partition(arr, low, high);
@@ -17,8 +17,35 @@ public class DualPivotQuick {
 	
 	
 	private static int[] partition(int[] arr, int low, int high) {
+		int l = low + 1; // left bound
+		int g = high - 1; // right bound
+		int k = low + 1; // current index
+
+		// low, high를 pivot1, pivot2로(pivot1 < pivot2)
+		if (arr[low] > arr[high])
+			swap(arr, low, high);
+		int pivot1 = arr[low];
+		int pivot2 = arr[high];
+
+		while (k <= g) {
+			if (arr[k] < pivot1)
+				swap(arr, l++, k);
+			else if (pivot2 < arr[k]) {
+				while (arr[g] >= pivot2 && k < g)
+					g--;
+
+				swap(arr, k, g--);
+
+				if (arr[k] < pivot1)
+					swap(arr, l++, k);
+			}
+			k++;
+		}
 		
-		return null;
+		swap(arr, --l, low);
+		swap(arr, ++g, high);
+		
+		return new int[] { l, g };
 	}
 
 
@@ -29,7 +56,7 @@ public class DualPivotQuick {
 	}
 	
 	public static void main(String[] args) {
-		int[] arr = { 2, 1};
+		int[] arr = { 40, 20, 50, 10, 30, 70 };
 		System.out.println(Arrays.toString(arr));
 
 		dualPivotQuickSort(arr, 0, arr.length - 1);
